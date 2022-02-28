20652238b13c03a3aebfeebe475307df22a9d362
しまじろう よう <you1025@gmail.com>
2022-02-28 18:51:04 +0900

    gensim.word2vec.Word2Vec のパラメータを変更

--- src/make_gensim_model.py
+++ src/make_gensim_model.py
@@ -4,9 +4,8 @@ from gensim.models import word2vec
 
 # モデルの作成
 data = word2vec.Text8Corpus("./data/jawiki_wakati.txt")
-model = word2vec.Word2Vec(data, vector_size=100, window=5, min_count=10, workers=4, sg=1, negative=5, epochs=1, seed=1025)
+model = word2vec.Word2Vec(data, vector_size=200, window=10, min_count=10, sg=1, negative=10, epochs=10, workers=8, seed=1025)
 
 # モデルの出力
 str_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 model.save(f"./models/jawiki_{str_now}.model")
-
