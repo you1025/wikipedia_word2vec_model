f5ba7e76a35fa6966847ca98bfd1403a5dde2488
しまじろう よう <you1025@gmail.com>
2022-03-01 10:19:31 +0900

    Word2Vec の実行パラメータを修正

--- src/make_gensim_model.py
+++ src/make_gensim_model.py
@@ -3,7 +3,10 @@ import datetime
 from gensim.models import word2vec
 from gensim.models.callbacks import CallbackAny2Vec
 
-class callback(CallbackAny2Vec):
+class PrintLoss(CallbackAny2Vec):
+    """
+    epoch 毎に Loss を算出して表示
+    """
     def __init__(self):
         self.epoch = 0
         self.cumulative_loss_to_prev_epoch = 0
@@ -11,6 +14,8 @@ class callback(CallbackAny2Vec):
     def on_epoch_end(self, model):
         self.epoch += 1
 
+        # Loss の算出
+        # model.get_latest_training_loss だと累積が出力されるらしいので通算 Loss を保持しておいて当該 epoch の値を算出 
         cumulative_loss = model.get_latest_training_loss()
         loss_on_latest_epoch = cumulative_loss - self.cumulative_loss_to_prev_epoch
 
@@ -20,8 +25,9 @@ class callback(CallbackAny2Vec):
 
 
 # モデルの作成
+# パラメータ一覧: https://radimrehurek.com/gensim/models/word2vec.html#gensim.models.word2vec.Word2Vec
 data = word2vec.Text8Corpus("./data/jawiki_wakati.txt")
-model = word2vec.Word2Vec(data, vector_size=100, window=5, min_count=10, workers=4, sg=1, negative=5, compute_loss=True, callbacks=[callback()], epochs=5, seed=1025)
+model = word2vec.Word2Vec(data, vector_size=200, window=10, min_count=5, workers=8, sg=1, negative=10, compute_loss=True, callbacks=(PrintLoss()), epochs=5, seed=1025)
 
 # モデルの出力
 str_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
